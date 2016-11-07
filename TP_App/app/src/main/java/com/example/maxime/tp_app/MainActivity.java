1f07a6bef2dadb8146105b227c557d9ce279475e
M2D_ISE01 <eom_mg_nab_udvb@tp_git.com>
2016-11-07 09:56:00 +0100

    Interface graphique 9h55 Uhu

--- TP_App/app/src/main/java/com/example/maxime/tp_app/MainActivity.java
+++ TP_App/app/src/main/java/com/example/maxime/tp_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.maxime.tp_app;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    if (mBluetoothAdapter == null) {
+        // Device does not support Bluetooth
     }
 }
