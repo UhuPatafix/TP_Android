3b120e98a6fe02ac2647eaa69134d7ee5bf5bbcc
M2D_ISE01 <eom_mg_nab_udvb@tp_git.com>
2016-11-07 12:29:45 +0100

    Application Bluetooth 12h29 Uhu

--- TP_App/app/src/main/java/com/example/maxime/tp_app/MainActivity.java
+++ TP_App/app/src/main/java/com/example/maxime/tp_app/MainActivity.java
@@ -1,19 +1,144 @@
 package com.example.maxime.tp_app;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Intent;
+
+import java.io.IOException;
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_ENABLE_BT = 0;
+    private Set<BluetoothDevice> devices;
+    BluetoothAdapter mBluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-    if (mBluetoothAdapter == null) {
-        // Device does not support Bluetooth
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(MainActivity.this, "Pas de Bluetooth", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Avec Bluetooth", Toast.LENGTH_SHORT).show();
+        }
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+        devices = mBluetoothAdapter.getBondedDevices();
+        for (BluetoothDevice blueDevice : devices) {
+            Toast.makeText(MainActivity.this, "Device = " + blueDevice.getName(), Toast.LENGTH_SHORT).show();
+        }}
+
+        // Permet de vérifier si le Bluetooth est déjà activé, sinon on demande à l'utilisateur d'activer le Bluetooth
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode != REQUEST_ENABLE_BT)
+                return;
+            if (resultCode == RESULT_OK) {
+                // L'utilisation a activé le bluetooth
+            } else {
+                // L'utilisation n'a pas activé le bluetooth
+            }
+        }
+
+        //Broadcast receiver permet de recevoir l'évènement qui se propage lorsqu'un nouveau device est détecté : BR sera averti lors de la détection d'un nouveau terminal
+        private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    Toast.makeText(MainActivity.this, "New Device = " + device.getName(), Toast.LENGTH_SHORT).show();
+                }
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(bluetoothReceiver, filter);
+
+                mBluetoothAdapter.startDiscovery();
+
+                // Pour rendre le device visible
+                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                startActivity(discoverableIntent);
+
+            }
+
+
+        };
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            BluetoothServerSocket tmp = null;
+            try {
+                tmp = mBluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME, MY_UUID);
+            } catch (IOException e) { }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            BluetoothSocket socket = null;
+            while (true) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    break;
+                }
+
+                if (socket != null) {
+                    manageConnectedSocket(socket);
+                    mmServerSocket.close();
+                    break;
+                }
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) { }
+        }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+            super.onDestroy();
+            mBluetoothAdapter.cancelDiscovery();
+            unregisterReceiver(bluetoothReceiver);
+
     }
 }
+
+
+
+
+
